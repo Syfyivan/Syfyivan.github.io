5676d15c852be8f9e44e5fa63a4118cdff1c8117
songyifan <yifansong@bytedance.com>
2026-08-08 11:08:25 +0800

    feat: publish interactive road trip maps

---
+++ source/road-trip-2026/assets/amap-config.local.example.js
@@ -0,0 +1,4 @@
+window.AMAP_LOCAL_CONFIG = {
+  jsKey: "请填写你的 Web端(JSAPI) key",
+  securityJsCode: "请填写你的安全密钥 jscode"
+};
---
+++ source/road-trip-2026/assets/amap-config.public.js
@@ -0,0 +1,4 @@
+window.AMAP_LOCAL_CONFIG = {
+  jsKey: "8c5cadba84651a091115324a97e1253b",
+  securityJsCode: "3bdeeb4eb19a4d0560cb3de4ce5368ff"
+};
